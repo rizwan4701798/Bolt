4a4bb4233c9a609005b99cdc5b3cf0ce5d3cf8be
Rizwan Farooq <rizwan.farooq@timesolv.onmicrosoft.com>
2022-12-16 17:04:28 +0500

    added basic s3 configuration

---
+++ tf-resources/terraform.tfvars
@@ -0,0 +1,9 @@
+bucket_name       = "s3bucket"
+acl_value         = "private"
+region            = "us-east-1"
+aws_access_key    = "AKIAZ6Q6AQQN2RUQDR4K"
+aws_access_secret = "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"
+
+
+
+
---
+++ tf-resources/variables.tf
@@ -0,0 +1,30 @@
+variable "bucket_name" {
+  type        = string
+  description = ""
+  default     = ""
+}
+
+variable "acl_value" {
+  type        = string
+  description = ""
+  default     = ""
+}
+
+variable "region" {
+      type        = string
+  description = ""
+  default     = ""
+}
+
+variable "aws_access_key" {
+  type        = string
+  description = ""
+  default     = ""
+}
+
+variable "aws_access_secret" {
+  type        = string
+  description = ""
+  default     = ""
+}
+
