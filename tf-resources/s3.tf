4d5e79c68aa3ce75758c82937575fe5cab609357
Rizwan Farooq <rizwan.farooq@timesolv.onmicrosoft.com>
2022-12-18 16:03:18 +0500

    Added gitignore and index.html

--- tf-resources/s3.tf
+++ tf-resources/s3.tf
@@ -2,3 +2,10 @@ resource "aws_s3_bucket" "demos3" {
     bucket = "${var.bucket_name}" 
     acl    = "${var.acl_value}"   
 }
+
+resource "aws_s3_bucket_object" "example" {
+  key        = "someobject"
+  bucket     = aws_s3_bucket.demos3.id
+  source     = "index.html"
+  kms_key_id = aws_kms_key.examplekms.arn
+}
